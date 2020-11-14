ab35efe767a40ed4b21edfd669f4e9b925834aae
langlan <langlan4744@gmail.com>
2020-11-14 22:56:55 +0800

    base framework: spring-boot-guid:accessing-data-rest

---
+++ src/main/java/com/github/langlan/AccessingDataRestApplication.java
@@ -0,0 +1,20 @@
+package com.github.langlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class AccessingDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataRestApplication.class, args);
+	}
+	
+	@RequestMapping("/t")
+	public String index() {
+		return "{\"msg\": \"hello world \"}";
+	}
+}
