1446953ea5ebef4698794bd72d1ad333458a03a8
langlan <langlan4744@gmail.com>
2020-12-06 19:15:34 +0800

    Dept: set type by if has major.

--- src/main/java/com/jytec/cs/dao/DeptRepository.java
+++ src/main/java/com/jytec/cs/dao/DeptRepository.java
@@ -1,7 +1,12 @@
 package com.jytec.cs.dao;
 
+import static com.jytec.cs.domain.Dept.DEPT_TYPE_ELSE;
+import static com.jytec.cs.domain.Dept.DEPT_TYPE_NORMAL;
+
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +17,11 @@ public interface DeptRepository extends PagingAndSortingRepository<Dept, Long> {
 
 	List<Dept> findByName(@Param("name") String name);
 
+	@Modifying
+	@Query("Update Dept d Set d.type='" + DEPT_TYPE_NORMAL + "' Where Exists (Select m From Major m Where m.dept=d)")
+	int updateTypeOfNormal();
+	
+	@Modifying
+	@Query("Update Dept d Set d.type='" + DEPT_TYPE_ELSE + "' Where Not Exists (Select m From Major m Where m.dept=d)")
+	int updateTypeOfElse();
 }
