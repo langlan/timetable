7083e61659d00d6af28af95ab3270d779623c5d9
langlan <langlan4744@gmail.com>
2020-12-13 18:05:43 +0800

    add a pom.war.xml and modify CsApplication for creating war.

--- src/main/java/com/jytec/cs/CsApplication.java
+++ src/main/java/com/jytec/cs/CsApplication.java
@@ -2,17 +2,26 @@ package com.jytec.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class CsApplication {
+public class CsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsApplication.class, args);
 	}
 	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // return application.sources(...);
+		System.out.println("...");
+		return builder;
+    }
+
 	@RequestMapping("/t")
 	public String index() {
 		return "{\"msg\": \"hello world \"}";
