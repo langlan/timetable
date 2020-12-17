498b2f8dd787405da8818e8fde58a368fdbc678a
langlan <langlan4744@gmail.com>
2020-12-17 16:52:46 +0800

    fix: forgotten commit file.

---
+++ src/main/java/com/jytec/cs/excel/exceptions/IllegalFormatException.java
@@ -0,0 +1,9 @@
+package com.jytec.cs.excel.exceptions;
+
+public class IllegalFormatException extends IllegalArgumentException {
+	private static final long serialVersionUID = 1L;
+
+	public IllegalFormatException(String msg) {
+		super(msg);
+	}
+}
